fdff3d51a8f0cd05d6aa2e6ff6eece3fa63138e1
Mrugank <mrugank1990@gmail.com>
2022-11-17 16:36:25 +0530

    Added extra code.

---
+++ src/strings/FirstNonRepeatingChar.java
@@ -0,0 +1,46 @@
+package strings;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+
+        String s = "test"; //op -> h [t,e,s,r
+
+        bruteForce(s);
+
+        // O(N) optimized
+        char uniqueChar = '_';
+        for (char c: s.toCharArray()){
+
+            if(s.indexOf(c) == s.lastIndexOf(c)){
+                //System.out.println(s.charAt(j));
+                uniqueChar = c;
+                break;
+            }
+        }
+
+        System.out.println(uniqueChar);
+
+
+    }
+
+    private static void bruteForce(String s) {
+
+        char uniqueChar = '_';
+
+        for(int i = 0; i < s.length(); i++){
+            boolean seenDuplicate = false;
+            for(int j = 0; j < s.length(); j++){
+
+                if(s.charAt(j) == s.charAt(i) && i != j){
+                    seenDuplicate = true;
+                    break;
+                }
+            }
+            if(!seenDuplicate){
+                uniqueChar = s.charAt(i);
+            }
+        }
+
+        System.out.println(uniqueChar);
+    }
+}
