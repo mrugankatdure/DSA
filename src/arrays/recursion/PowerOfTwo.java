1a360b0a63f96a0ab4233c84562a433bd4fb1719
mrugank <mrugank1990@gmail.com>
2022-11-18 18:30:06 +0530

    Added code for power of 2 under recursion.

--- src/arrays/recursion/PowerOfTwo.java
+++ src/arrays/recursion/PowerOfTwo.java
@@ -4,5 +4,20 @@ public class PowerOfTwo {
 
     public static void main(String[] args) {
 
+        int base = 2, exponent = 2;
+
+        //calculate power of 2 using recursion
+        int result = powerOfTwo(base, exponent);
+        System.out.println(result);
+
+    }
+
+    private static int powerOfTwo(int base, int exponent) {
+
+        //base case here
+        if(exponent == 0) return 1;
+
+        return base * powerOfTwo(base, exponent - 1 );
+
     }
 }
