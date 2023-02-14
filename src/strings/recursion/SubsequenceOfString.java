166ca6734c9574f00421d16fcee4704d446088d6
mrugank <mrugank.gadgil@credable.in>
2023-02-14 14:37:31 +0530

    Check if string is subsequence of another string.

--- src/strings/recursion/SubsequenceOfString.java
+++ src/strings/recursion/SubsequenceOfString.java
@@ -3,6 +3,25 @@ package strings.recursion;
 public class SubsequenceOfString {
     public static void main(String[] args) {
 
-        String s1 = "abcdabe", s2 = "dab"; //check if s2 is subsequence of s1
+        String s1 = "abcde", s2 = "bc"; //check if s2 is subsequence of s1
+        System.out.println(isSubsequence(s1, s2));
+
+    }
+
+
+    private static boolean isSubsequence(String s1, String s2) {
+
+        //base case here
+        if(s1.length() == 0 || s2.length() == 0) return true;
+        if(s2.length() == 0 && s1.length() != 0) return false;
+        // check if last character of s2 matches with s1
+        if(s2.charAt(s2.length()-1) == s1.charAt(s1.length()-1)) {
+            //recur without last char of s1
+            return isSubsequence(s1.substring(0,s1.length()-1), s2.substring(0,s2.length() - 1));
+        } else {
+            return isSubsequence(s1.substring(0,s1.length()-1), s2);
+        }
+
+
     }
 }
